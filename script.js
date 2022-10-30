a45637f52efea03985c5eb7bf6fa62f0e043f59d
Your Name <you@example.com>
2022-10-30 16:00:07 +0100

    m

---
+++ script.js
@@ -0,0 +1,791 @@
+const quizData = [
+    {
+        question: "1. Jablko je zelenina.",
+        a: "pravda",
+        b: "lež",
+        
+        correct: "b",
+    },
+    {
+      question: "2. Anakonda je pták.",
+      a: "pravda",
+      b: "lež",
+      
+      correct: "b",
+  },
+  {
+    question: "3. Slovensko je v  Americe.",
+    a: "pravda",
+    b: "lež",
+    
+    correct: "b",
+},
+{
+  question: "4. Listy na podzim padají ze stromů.",
+  a: "pravda",
+  b: "lež",
+  
+  correct: "a",
+},{
+  question: "5. Svokra je máma manžela nebo manželky.",
+  a: "pravda",
+  b: "lež",
+  
+  correct: "a",
+},
+{
+question: "6. Setřenice je dcera sestry.",
+a: "pravda",
+b: "lež",
+
+correct: "b",
+},{
+  question: "7. Mravenec je chutná zelenina.",
+  a: "pravda",
+  b: "lež",
+  
+  correct: "b",
+},
+{
+question: "8. Bambi byl malý talířek.",
+a: "pravda",
+b: "lež",
+
+correct: "b",
+},{
+  question: "9. Božena Němcová byla první česká královna.",
+  a: "pravda",
+  b: "lež",
+  
+  correct: "b",
+},
+{
+question: "10. Papír hoří.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},{
+  question: "11. Po pondělí jde úterý.",
+  a: "pravda",
+  b: "lež",
+  
+  correct: "a",
+},
+{
+question: "12. Pluto je trpasličí planeta.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},{
+  question: "13. Magnet přitahuje ponožky.",
+  a: "pravda",
+  b: "lež",
+  
+  correct: "b",
+},
+{
+question: "14. Krev obsahuje červené krvinky.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},{
+  question: "15. Školní rok začíná v září.",
+  a: "pravda",
+  b: "lež",
+  
+  correct: "a",
+},
+{
+question: "16. Všechny děti umí létat.",
+a: "pravda",
+b: "lež",
+
+correct: "b",
+},{
+  question: "17. Lednice je hudební nástroj.",
+  a: "pravda",
+  b: "lež",
+  
+  correct: "a",
+},
+{
+question: "18. Benzin se vyrábí z ropy.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},{
+  question: "19. Smícháním modré a zelené vznikne tyrkysová.",
+  a: "pravda",
+  b: "lež",
+  
+  correct: "a",
+},
+{
+question: "20. Jahody padají ze stromů.",
+a: "pravda",
+b: "lež",
+
+correct: "b",
+},
+{
+  question: "21. Berušky mají 20 noh.",
+  a: "pravda",
+  b: "lež",
+  
+  correct: "b",
+},
+{
+question: "22. Hlavní přísadou do koláčů je písek.",
+a: "pravda",
+b: "lež",
+
+correct: "b",
+},
+{
+question: "23. Panenky umí mluvit.",
+a: "pravda",
+b: "lež",
+
+correct: "b",
+},
+{
+question: "24. Zelená na semaforu znamená, že můžeme jít.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},{
+question: "5. Včely sbírají pyl a dělají z něj marmeládu.",
+a: "pravda",
+b: "lež",
+
+correct: "b",
+},
+{
+question: "6. Hříbátko je mládětem pejska.",
+a: "pravda",
+b: "lež",
+
+correct: "b",
+},{
+question: "7. Broskev je chutná zelenina.",
+a: "pravda",
+b: "lež",
+
+correct: "b",
+},
+{
+question: "8. Bambi byl malý koloušek.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},{
+question: "9. Zmrzlina se dělá z křídy.",
+a: "pravda",
+b: "lež",
+
+correct: "b",
+},
+{
+question: "10. Lepící páska se používá na slepování papíru.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},{
+question: "11. Voda se vypařuje a mění se na páru.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},
+{
+question: "12. Zem se otáčí kolem slunce.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},{
+question: "13. Magnet přitahuje železné předměty.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},
+{
+question: "14. Lidé potřebují kyslík na dýchání.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},{
+question: "15. Pták se líhne z vajíčka.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},
+{
+question: "16. Všechny děti se učí počítat.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},{
+question: "17. Klavír je hudební nástroj.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},
+{
+question: "18. Knížky se vyrábí z kakaa.",
+a: "pravda",
+b: "lež",
+
+correct: "b",
+},{
+question: "19. Leden je první měsíc v roku.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},
+{
+question: "20. Listy padají ze stromů.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},
+{
+  question: "1. Hrušky jsou velká auta.",
+  a: "pravda",
+  b: "lež",
+  
+  correct: "b",
+},
+{
+question: "2. V bance se peče chleba.",
+a: "pravda",
+b: "lež",
+
+correct: "b",
+},
+{
+question: "3. Panenky umí mluvit.",
+a: "pravda",
+b: "lež",
+
+correct: "b",
+},
+{
+question: "4. Zelená na semaforu znamená, že můžeme jít.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},{
+question: "5. Včely sbírají pyl a dělají z něj marmeládu.",
+a: "pravda",
+b: "lež",
+
+correct: "b",
+},
+{
+question: "6. Hříbátko je mládětem pejska.",
+a: "pravda",
+b: "lež",
+
+correct: "b",
+},{
+question: "7. Broskev je chutná zelenina.",
+a: "pravda",
+b: "lež",
+
+correct: "b",
+},
+{
+question: "8. Bambi byl malý koloušek.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},{
+question: "9. Zmrzlina se dělá z křídy.",
+a: "pravda",
+b: "lež",
+
+correct: "b",
+},
+{
+question: "10. Lepící páska se používá na slepování papíru.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},{
+question: "11. Voda se vypařuje a mění se na páru.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},
+{
+question: "12. Zem se otáčí kolem slunce.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},{
+question: "13. Magnet přitahuje železné předměty.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},
+{
+question: "14. Lidé potřebují kyslík na dýchání.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},{
+question: "15. Pták se líhne z vajíčka.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},
+{
+question: "16. Všechny děti se učí počítat.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},{
+question: "17. Klavír je hudební nástroj.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},
+{
+question: "18. Knížky se vyrábí z kakaa.",
+a: "pravda",
+b: "lež",
+
+correct: "b",
+},{
+question: "19. Leden je první měsíc v roku.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},
+{
+question: "20. Listy padají ze stromů.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},
+{
+  question: "1. Hrušky jsou velká auta.",
+  a: "pravda",
+  b: "lež",
+  
+  correct: "b",
+},
+{
+question: "2. V bance se peče chleba.",
+a: "pravda",
+b: "lež",
+
+correct: "b",
+},
+{
+question: "3. Panenky umí mluvit.",
+a: "pravda",
+b: "lež",
+
+correct: "b",
+},
+{
+question: "4. Zelená na semaforu znamená, že můžeme jít.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},{
+question: "5. Včely sbírají pyl a dělají z něj marmeládu.",
+a: "pravda",
+b: "lež",
+
+correct: "b",
+},
+{
+question: "6. Hříbátko je mládětem pejska.",
+a: "pravda",
+b: "lež",
+
+correct: "b",
+},{
+question: "7. Broskev je chutná zelenina.",
+a: "pravda",
+b: "lež",
+
+correct: "b",
+},
+{
+question: "8. Bambi byl malý koloušek.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},{
+question: "9. Zmrzlina se dělá z křídy.",
+a: "pravda",
+b: "lež",
+
+correct: "b",
+},
+{
+question: "10. Lepící páska se používá na slepování papíru.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},{
+question: "11. Voda se vypařuje a mění se na páru.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},
+{
+question: "12. Zem se otáčí kolem slunce.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},{
+question: "13. Magnet přitahuje železné předměty.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},
+{
+question: "14. Lidé potřebují kyslík na dýchání.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},{
+question: "15. Pták se líhne z vajíčka.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},
+{
+question: "16. Všechny děti se učí počítat.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},{
+question: "17. Klavír je hudební nástroj.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},
+{
+question: "18. Knížky se vyrábí z kakaa.",
+a: "pravda",
+b: "lež",
+
+correct: "b",
+},{
+question: "19. Leden je první měsíc v roku.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},
+{
+question: "20. Listy padají ze stromů.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},
+{
+  question: "1. Hrušky jsou velká auta.",
+  a: "pravda",
+  b: "lež",
+  
+  correct: "b",
+},
+{
+question: "2. V bance se peče chleba.",
+a: "pravda",
+b: "lež",
+
+correct: "b",
+},
+{
+question: "3. Panenky umí mluvit.",
+a: "pravda",
+b: "lež",
+
+correct: "b",
+},
+{
+question: "4. Zelená na semaforu znamená, že můžeme jít.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},{
+question: "5. Včely sbírají pyl a dělají z něj marmeládu.",
+a: "pravda",
+b: "lež",
+
+correct: "b",
+},
+{
+question: "6. Hříbátko je mládětem pejska.",
+a: "pravda",
+b: "lež",
+
+correct: "b",
+},{
+question: "7. Broskev je chutná zelenina.",
+a: "pravda",
+b: "lež",
+
+correct: "b",
+},
+{
+question: "8. Bambi byl malý koloušek.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},{
+question: "9. Zmrzlina se dělá z křídy.",
+a: "pravda",
+b: "lež",
+
+correct: "b",
+},
+{
+question: "10. Lepící páska se používá na slepování papíru.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},{
+question: "11. Voda se vypařuje a mění se na páru.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},
+{
+question: "12. Zem se otáčí kolem slunce.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},{
+question: "13. Magnet přitahuje železné předměty.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},
+{
+question: "14. Lidé potřebují kyslík na dýchání.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},{
+question: "15. Pták se líhne z vajíčka.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},
+{
+question: "16. Všechny děti se učí počítat.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},{
+question: "17. Klavír je hudební nástroj.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},
+{
+question: "18. Knížky se vyrábí z kakaa.",
+a: "pravda",
+b: "lež",
+
+correct: "b",
+},{
+question: "19. Leden je první měsíc v roku.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},
+{
+question: "20. Listy padají ze stromů.",
+a: "pravda",
+b: "lež",
+
+correct: "a",
+},
+    
+  ];
+  
+  const quiz = document.getElementById('quiz')
+  const answerEls = document.querySelectorAll('.answer')
+  const questionEl = document.getElementById('question')
+  const result = document.getElementById('resultquestion')
+  const a_text = document.getElementById('a_text')
+  const b_text = document.getElementById('b_text')
+  
+  const submitBtn = document.getElementById('submit')
+  
+  let currentQuiz = 0
+  let score = 0
+  
+  loadQuiz()
+  
+  function loadQuiz() {
+    deselectAnswers()
+  
+    const currentQuizData = quizData[currentQuiz]
+  
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+   
+  }
+  
+  function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+  }
+  
+  function getSelected() {
+    let answer
+  
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+  
+    return answer
+  }
+  let answers= []; 
+  submitBtn.addEventListener('click', () => {
+    const answer = getSelected()
+    
+    if(answer) { answers.push(answer);
+            console.log(answers);}
+  
+    if(answer) {
+        if(answer === quizData[currentQuiz].correct) {
+            score++;
+            
+            
+        }
+        
+        currentQuiz++;
+        
+  
+        if(currentQuiz < quizData.length) {
+            loadQuiz()
+        } else {
+            
+            
+  /* 
+  */
+           let results= quizData.map ((question,i) => 
+           
+           
+           ` 
+                <div class="${quizData[i].correct===answers[i]?
+                    "correct": "false" } quiz-header ">
+                <h2 id="question">${question.question}</h2>
+                <ul id="resultquestion"  >
+                  <li >
+                    <input type="radio" name="answer" id="a" class="answer">
+                    <label for="a" id="a_text">${question.a}</label>
+                  </li>
+        
+                  <li>
+                    <input type="radio" name="answer" id="b" class="answer">
+                    <label for="b" id="b_text">${question.b}</label>
+                  </li>
+        
+                  
+                  <li>
+                  <h4>Správná odpověď: ${question[quizData[i].correct]}</h4>
+                <h4>Vybral jsi: ${question[answers[i]]}</h4>
+  
+                
+                  </li>
+                  
+                </ul>
+              </div>`
+                
+                )
+  
+  //                 result.classList.add("correct")
+  // /* 
+                
+  
+                /* quizData.forEach ((question,i) =>
+                 quizData[i].correct===answers[i]?
+                  result.classList.add("correct")  : result.classList.add("false") 
+                 ) */
+            
+         
+  
+  
+            quiz.innerHTML = `
+                <h2>Získal jsi ${score} bodů z ${quizData.length}.</h2>
+               
+  
+                
+                
+               
+    
+                ${results}
+  
+  
+                
+  
+                <button onclick="location.reload()">Znovu zkusit</button>
+            `
+        
+        
+        }
+    }
+  })
